a54f7d3bcff6b2a662660d21def8a6189ead7c19
Marc <marcsr03@gmail.com>
2021-06-22 19:04:33 +0200

    added html code and styles.css. Styles are incomplete, but sufficient to start working properly for now.

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/styles.css">
+</head>
+<body>
+    <header>
+        <div class="headerLeft"> logo </div>
+        <div class="headerCenter"> 
+            <input class="searchBar" type="text">
+        </div>
+        <div class="headerRight"> 
+            <div class="icon"> view </div>
+            <div class="icon"> trash </div>
+            <div class="icon"> user </div>
+        </div>
+    </header>
+    <main>
+        <section class="mainLeft">
+            <div class="newFile"> 
+                <button class ="" type="button"> + </button>
+            </div>
+            <hr>
+            <div class="fdContainer">
+                <div> File </div>
+                <div> Directory </div>
+            </div>
+        </section>
+        <section class="mainCenter">
+            <div class ="fileWrapper">
+                <div class="fileIcon">icon</div>
+                <div class="fileName">filename</div>
+            </div>
+        </section>
+        <section class="mainRight">
+            <div class="fileHeader">
+                <div class="fileIcon">icon</div>
+                <div class="fileName">filename</div>
+            </div>
+            <div class="fileInfoContainer">
+                <div class="fileInfoWrapper">
+                    <div class="label"> Label</div>
+                    <div class="labelType">APPROVED</div>
+                </div>
+                <div class="fileInfoWrapper">
+                    <div class="label"> Type</div>
+                    <div class="labelType">MSExcel Document</div>
+                </div>
+                <div class="fileInfoWrapper">
+                    <div class="label"> Size</div>
+                    <div class="labelType">38 KB</div>
+                </div>
+                <div class="fileInfoWrapper">
+                    <div class="label"> Modified</div>
+                    <div class="labelType">Feb 23, 2016</div>
+                </div>
+                <div class="fileInfoWrapper">
+                    <div class="label"> Created</div>
+                    <div class="labelType">00:00</div>
+                </div>
+                <div class="fileInfoWrapper">
+                    <div class="label"> Rating</div>
+                    <div class="labelType">Stars</div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
