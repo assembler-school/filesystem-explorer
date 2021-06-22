e63204582b6463f27b1f5f3652d57570aaf4ac87
labietelabiete <victorhugogote@gmail.com>
2021-06-22 15:36:10 +0200

    Firsts folders test

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php
+$myPath = "C:/xampp/htdocs/Assembler/Projects/02-php-file-manager/filesystem-explorer/root";
+$rootPath = "C:/xampp/htdocs/Assembler/Projects/02-php-file-manager/filesystem-explorer/root";
+echo $myPath;
+echo "<br/>";
+
+// Getting last folder
+$lastFolder = basename($myPath);
+echo $lastFolder;
+echo "<br/>";
+
+// Adding path
+$pathAdded = "/firstPath";
+$myPath .= $pathAdded;
+echo $myPath;
+echo "<br/>";
+
+// Creating directory
+mkdir($myPath, 0700);
+
+// Removing directory
+rmdir($myPath);
+
+// is_dir
+if (is_dir($myPath)) {
+  echo "Existe el directorio";
+}
+echo "<br/>";
+
+// Getting files and folders from directory
+$myDir = scandir($rootPath);
+print_r($myDir);
+
+
+
+?>
+
 <html lang="en">
 
 <head>
@@ -9,7 +46,14 @@
 </head>
 
 <body>
-    Hola 
+  <form class="form-login row" action="./src/creatingFolder.php" method="POST">
+    <div class="col col-6">
+      <input type="text" name="folderName" placeholder="Folder name" id="folderName" class="form-control">
+    </div>
+    <div class="col col-6">
+      <button class="btn btn-primary" type="submit">New Folder</button>
+    </div>
+  </form>
   <script src="node_modules\bootstrap\dist\js\bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
