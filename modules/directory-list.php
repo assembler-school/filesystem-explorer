555c619bac18a84da38489badd8cfa5ee4f1d764
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-06-23 10:56:38 +0200

    Change date format

--- modules/directory-list.php
+++ modules/directory-list.php
@@ -33,13 +33,15 @@ function human_filesize($bytes, $decimals = 2)
 $foldersArray = array_filter($scannedDirectory, "isDir");
 $filesArray = array_filter($scannedDirectory, "isFile");
 
+$dateFormat = "m/d/Y H:i";
+
 foreach ($foldersArray as $folder) {
     echo ("<tr><th scope='row'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-folder' viewBox='0 0 16 16'>
         <path d='M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z'/>
         </svg></th>
         <td>" . $folder . "</td><td></td>
-        <td>" . date("F d Y H:i:s.", filemtime($folder)) . "</td>
-        <td>" . date("F d Y H:i:s.", filectime($folder)) . "</td></tr>");
+        <td>" . date($dateFormat, filemtime($folder)) . "</td>
+        <td>" . date($dateFormat, filectime($folder)) . "</td></tr>");
 }
 
 foreach ($filesArray as $file) {
@@ -47,7 +49,7 @@ foreach ($filesArray as $file) {
                             <th scope='row'>" . fileIcon($file) . "</th>
                             <td><span>" . explode(".", $file)[0] . "</span><span class='text-uppercase text-black-50 p-2' style='font-size: 0.8rem'>" .  explode(".", $file)[1] . "</span>" . "</td>
                             <td>" . human_filesize(filesize($file)) . "</td>
-                            <td>" . date("F d Y H:i:s.", filemtime($file)) . "</td>
-                            <td>" . date("F d Y H:i:s.", filectime($file)) . "</td>
+                            <td>" . date($dateFormat, filemtime($file)) . "</td>
+                            <td>" . date($dateFormat, filectime($file)) . "</td>
                         </tr> ";
 }
