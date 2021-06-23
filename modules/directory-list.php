612bcaa59168d7e800f0572f0c49c1762168a9ac
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-06-23 09:53:34 +0200

    Finished file icon implementation

--- modules/directory-list.php
+++ modules/directory-list.php
@@ -1,4 +1,6 @@
 <?php
+require_once("./modules/file-icon.php");
+
 // Change current directory to root
 $homeDir = "./root";
 chdir($homeDir);
@@ -42,9 +44,9 @@ foreach ($foldersArray as $folder) {
     // echo " was last modified: " . date("F d Y H:i:s.", filemtime($folder)) . "<br>";
     // echo " was created: " . date("F d Y H:i:s.", filectime($folder)) . "<br>"; // Doesn't work on Unix
     echo ("<tr>
-                            <th scope='row'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-folder-fill' viewBox='0 0 16 16'>
-                                    <path d='M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z' />
-                                </svg></th>
+                            <th scope='row'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-folder' viewBox='0 0 16 16'>
+  <path d='M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z'/>
+</svg></th>
                             <td>" . $folder . "</td>
                             <td></td>
                             <td>" . date("F d Y H:i:s.", filemtime($folder)) . "</td>
@@ -58,10 +60,7 @@ foreach ($filesArray as $file) {
     echo " was created: " . date("F d Y H:i:s.", filectime($file)) . "<br>"; // Doesn't work on Unix
     echo  " size:" . human_filesize(filesize($file)) . "<br>"; */
     echo "<tr>
-                            <th scope='row'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-file-earmark-image-fill' viewBox='0 0 16 16'>
-                                    <path d='M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707v5.586l-2.73-2.73a1 1 0 0 0-1.52.127l-1.889 2.644-1.769-1.062a1 1 0 0 0-1.222.15L2 12.292V2a2 2 0 0 1 2-2zm5.5 1.5v2a1 1 0 0 0 1 1h2l-3-3zm-1.498 4a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z' />
-                                    <path d='M10.564 8.27 14 11.708V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-.293l3.578-3.577 2.56 1.536 2.426-3.395z' />
-                                </svg></th>
+                            <th scope='row'>" . fileIcon($file) . "</th>
                             <td>" . explode(".", $file)[0] .  explode(".", $file)[1] . "</td>
                             <td>" . human_filesize(filesize($file)) . "</td>
                             <td>" . date("F d Y H:i:s.", filemtime($file)) . "</td>
