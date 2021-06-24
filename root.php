349d305f7736e4c267e0da05243de482b7c0f851
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-06-24 15:12:00 +0200

    navbar & sohwformButon

--- root.php
+++ root.php
@@ -4,20 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css"> -->
+   <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <link rel="stylesheet" href="./styles.css">
     <title>Document</title>
 </head>
   <body class="bod">
-  <form  action="./dirManege/create.php" method="post">
+  <form class="newFolderForm" action="./dirManege/create.php" method="post">
         <input type="text" name="dirname" id="submitButon">
         <input type="submit" >
     </form>
       
-    <header class="navBar"></header>
+    <header class="navBarContain">
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand">Navbar</a>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </header>
     <main>
-      <section class="aside"></section>
+      <section class="aside">
+        <button  class="newFolderButon">+</button>
+      </section>
             
       <section class="folders">
           <?php
@@ -30,7 +42,7 @@
       </section>
       </main>
  
-  <!-- 
-  <script src="script.js"></script> -->
+  
+  <script src="script.js"></script>
   </body>
 </html>
--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+$(".newFolderForm").hide();
+$(".newFolderButon").on("click",()=>{
+    if($(".newFolderForm").hide()){
+        $(".newFolderForm").show();
+    }else{
+        $(".newFolderForm").hide();
+    }
+  }
+)
 
-let deleteButtons = document.querySelectorAll(".buton")
-console.log(deleteButtons)
+// let deleteButtons = document.querySelectorAll(".buton")
+// console.log(deleteButtons)
 
  
-deleteButtons.forEach((each)=>{
-   each.addEventListener("submit",(e)=>{
-       e.preventDefault();
-       console.log(e.target)
+// deleteButtons.forEach((each)=>{
+//    each.addEventListener("submit",(e)=>{
+//        e.preventDefault();
+//        console.log(e.target)
     
-    $.ajax({
-        url: "./root.php",
-        type:"POST",
-        //data:new FormData(this),
-        contentType: false,
-    	cache: false,
-        processData:false,
-        success: function(response)
-      {console.log(response);}
+//     $.ajax({
+//         url: "./root.php",
+//         type:"POST",
+//         //data:new FormData(this),
+//         contentType: false,
+//     	cache: false,
+//         processData:false,
+//         success: function(response)
+//       {console.log(response);}
       
-   });
-})
-})
+//    });
+// })
+// })
