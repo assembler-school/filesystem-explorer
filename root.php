5ae9034ba2850505ca2d30729d68e0a8643ac07a
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-06-24 13:50:22 +0200

    styling

--- root.php
+++ root.php
@@ -4,26 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles.css">
+   <!-- <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css"> -->
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="./styles.css">
     <title>Document</title>
 </head>
-  <body>
+  <body class="bod">
   <form  action="./dirManege/create.php" method="post">
         <input type="text" name="dirname" id="submitButon">
         <input type="submit" >
     </form>
-    
-        <input type="text" name="newname" id="NameButon">
-        <a href=´./dirManege/edit.php?hola´>ddd</a>
-    
-    <?php
-      require("./functions/dirManege.php");
-        scan();
-        if(isset($_SESSION["existingFolder"])){
-          echo $_SESSION["existingFolder"];
-        }    
-    ?>
+  
+      <section class="navBar"></section>
+      <section class="aside"></section>
+        
+      <section class="folders">
+        <?php
+          require("./functions/dirManege.php");
+            scan();
+            if(isset($_SESSION["existingFolder"])){
+              echo $_SESSION["existingFolder"];
+            }    
+        ?>
+      </section>
+ 
   <!-- 
   <script src="script.js"></script> -->
   </body>
