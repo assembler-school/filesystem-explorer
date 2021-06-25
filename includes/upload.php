4dc7bb51ce8887b06d26526763b1462b55e908fd
Marc <marcsr03@gmail.com>
2021-06-25 16:38:47 +0200

    upload file works for now, but need to implement a better solution to avoid file replacement when file to upload name is the same as an existing file in the directory to which the upload is being made.

--- includes/upload.php
+++ includes/upload.php
@@ -23,8 +23,14 @@
             // Checking if there was an error ($fileError === 0 means no error)
             if($fileError === 0){
                 // Checking file size (for now file size limit set at 1000000)
-                if($fileSize < 1000000){
-                    $fileNameNew = uniqid('', true);
+                if($fileSize < 10000000){
+                    // Creating unique id for the file to avoid replacement issues
+                    // and also creating the file upload destination and redirect to
+                    // index.php once the process has finished
+                    $fileNameNew = uniqid('', true).".".$fileActualExt;
+                    $fileDestination = '../root/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    header("Location: ../index.php?uploadsuccess");
                 }else{
                     echo "Your file is too big!";
                 }
