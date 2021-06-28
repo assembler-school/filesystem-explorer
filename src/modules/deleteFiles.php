d3ebb18fc826f1eff458b730219c9b58c7b5ca37
Ricard Garcia <imricardgarcia@gmail.com>
2021-06-28 16:46:06 +0200

    feat: added delete file/directory functionality

--- src/modules/deleteFiles.php
+++ src/modules/deleteFiles.php
@@ -1,108 +1,35 @@
 <?php
+// session_start();
+
+$nameToDelete = $_POST["fileName"];
+$typeToDelete = $_POST["fileType"];
+$pathToDelete =  dirname(getcwd()) . "/root/" . $nameToDelete;
+
+echo "Deleted file: " . $nameToDelete . "<br>";
+echo "Deleted type: " . $typeToDelete . "<br>";
+echo $pathToDelete;
+
+
+if (!is_dir($pathToDelete)) {
+    unlink($pathToDelete);
+    echo "Deleted file";
+} else {
+    deleteDir($pathToDelete);
+}
 
 
 
-// delete 
+function deleteDir($path_file)
+{
+    if (is_dir($path_file)) {
+        $files = array_diff(scandir($path_file), [".", ".."]);
+        foreach ($files as $file)
+            deleteDir("$path_file/$file");
+        rmdir($path_file);
+    } else {
+        unlink($path_file);
+    }
+}
 
-session_start();
-
-
-$pathToDelete = $_GET["delete"];
-
-$path_file =  dirname(getcwd()) . "/root/" . $pathToDelete;
-
-
-// unlink($path_file);
-// rmdir($path_file);
 
 header("Location:../index.php");
-
-
-
-// function deleteDir($path_file)
-// {
-//     if (is_dir($path_file)) {
-//         $files = array_diff(scandir($path_file), [".", ".."]);
-//         foreach ($files as $file)
-//             deleteDir("$path_file/$file");
-//         rmdir($path_file);
-//     } else {
-//         unlink($path_file);
-//     }
-// }
-
-
-
-
-
-// $pathToDelete = dirname(__FILE__) . "/" . $fileName;
-
-// $fileName = $_GET["delete"];
-
-// unlink($pathToDelete);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $rootPath = "./root";
-
-// function deleteFile($pathToDelete)
-// {
-//     if (is_link($pathToDelete)) {
-//         return unlink($pathToDelete);
-//     } elseif (is_dir($pathToDelete)) {
-//         $objets = scandir($pathToDelete);
-//         $ok = true;
-//         if (is_array($objets)) {
-//             foreach ($objets as $j) {
-//                 if ($j != "." && $j != "..") {
-//                     if (!deleteFile($pathToDelete . "/" . $j)) {
-//                         $ok = false;
-//                     }
-//                 }
-//             }
-//         }
-//         return ($ok) ? rmdir($pathToDelete) : false;
-//     } elseif (is_file($pathToDelete)) {
-//         return unlink($pathToDelete);
-//     }
-
-//     return false;
-// }
-
-
-
-// function deleteDir($pathToDelete)
-// {
-//     if (is_dir($pathToDelete)) {
-//         $objects = scandir($pathToDelete);
-//         foreach ($objects as $i) {
-//             if ($i != "." && $i != "..") {
-//                 if (filetype($pathToDelete . "/" . $i) == "name") deleteDir($pathToDelete . "/" . $i);
-//                 else unlink($pathToDelete . "/" . $i);
-//             }
-//         }
-//         reset($objects);
-//         rmdir($pathToDelete);
-//     }
-// }
--- src/modules/directoryFiles.php
+++ src/modules/directoryFiles.php
@@ -23,7 +23,7 @@ foreach (scandir($target_dir) as $i) {
                 echo "<a class='dir dir-link dir-child row file-item px-3 py-2 d-flex justify-content-center align-items-center' href=./index.php?filePath=" . $_SESSION["currentPath"] . "/" . $fileArray["name"] . "&fileName=" . $fileArray["name"] . ">";
             }
             // echo "<a class='row file-item px-3 py-2 d-flex justify-content-center align-items-center'>";
-            echo "<div class='row col col-6 p-0 icon-and-name d-flex justify-content-between align-items-center'>";
+            echo "<div class='row col col-4 p-0 icon-and-name d-flex justify-content-between align-items-center'>";
             echo "<p class='col col-2 file-text file-icon p-0 d-flex justify-content-center'>";
             echo "<i class='far fa-" . $fileArray["icon"] . "'></i>";
             echo "</p>";
@@ -36,20 +36,39 @@ foreach (scandir($target_dir) as $i) {
             echo "<p class='col col-2 file-text'>" . $fileArray["size"] . "</p>";
             echo "<p class='col col-2 file-text'>" . $fileArray["creation"] . "</p>";
             echo "<p class='col col-2 file-text'>" . $fileArray["modification"] . "</p>";
+            // Delete button link
+            echo "<div class='row col col-2 file-buttons file-text'>";
+
+            echo "<form class='col col-6' method='POST' action='./modules/editFiles.php?donwload=" . $fileArray["name"] . "'>";
+            echo "<input class='button-file' name='fileName' value='" . $fileArray["name"] . "'/>";
+            echo "<input class='button-file' name='fileType' value='" . $fileArray["name"] . "'/>";
+            echo "<button type='submit' class='btn col col-6'>";
+            echo "<i class='fas fa-file-download'></i>";
+            echo "</button>";
+            echo "</form>";
+
+
+            echo "<form class='col col-6' method='POST' action='./modules/deleteFiles.php'>";
+            echo "<input class='button-file' name='fileName' value='" . $fileArray["name"] . "'/>";
+            echo "<input class='button-file' name='fileType' value='" . $fileArray["type"] . "'/>";
+            echo "<button type='submit' class='btn col col-6'>";
+            echo "<i class='far fa-trash-alt'></i>";
+            echo "</button>";
+            echo "</form>";
+
+
+            echo "</div>";
             echo "</a>";
         }
     }
 };
 
 
-// Delete button link
-echo "<a href=./modules/deleteFiles.php?delete=" . $fileArray["name"] . ">delete</a>";
+
 
 // Download
 
-echo "<br>";
 
-echo "<a href=./modules/editFiles.php?donwload=" . $fileArray["name"] . ">download</a>";
 
 /* -------------------------------------------------------------------------- */
 /*                                    TEST                                    */
