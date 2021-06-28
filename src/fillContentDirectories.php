51ec61c731ac74be5a0a39c0f26187962f71866c
Alberto <gs89alberto@gmail.com>
2021-06-28 09:20:38 +0200

    fix

--- src/fillContentDirectories.php
+++ src/fillContentDirectories.php
@@ -8,11 +8,11 @@ require_once('./src/fillContent.php');
 
 $folders = getDirectories($_SESSION['currentPath']);
 
-array_walk($folders, function($path) {
-    $folderName = basename($path);
-    $shortFolderName = strlen($folderName)>16 ? substr($folderName, 0, 13) . '...' : $folderName;
-    $filesCount = count(scandir($path)) - 2;
-    echo "<div class='folder d-flex flex-column justify-content-end mx-2 p-1' title='$folderName'>
+array_walk($folders, function ($path) {
+  $folderName = basename($path);
+  $shortFolderName = strlen($folderName) > 16 ? substr($folderName, 0, 13) . '...' : $folderName;
+  $filesCount = count(scandir($path)) - 2;
+  echo "<div class='folder d-flex flex-column justify-content-end mx-2 p-1' title='$folderName'>
     <p class='m-0'>$shortFolderName</p>
     <p class='m-0'>Items: $filesCount</p>
   </div>";
--- src/fillContentFiles.php
+++ src/fillContentFiles.php
@@ -15,16 +15,14 @@ $files = getFiles($_SESSION['currentPath']);
 
 array_walk($files, function($filePath) {
     $fileName = basename($filePath);
-    $shortFileName = strlen($fileName)>16 ? substr($fileName, 0, 13) . '...' : $fileName;
+    $shortFileName = strlen($fileName)>22 ? substr($fileName, 0, 18) . '...' : $fileName;
     $fileSize = human_filesize(filesize($filePath));
     echo "<div class='file d-flex flex-column justify-content-end mx-2 p-1' title='$fileName'>
     <div class='file-top'>
-
     </div>
     <div class='file-bottom'>
-      <p class='m-0 text-wrap'>$shortFileName</p>
-      <p class='m-0 text-wrap'>$fileSize</p>
+      <p class='mx-1 my-0 d-flex justify-content-center'>$shortFileName</p>
+      <p class='mx-1 my-0 d-flex justify-content-end'>$fileSize</p>
     </div>
-
   </div>";
 });
