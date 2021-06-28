faa3fde5d3091da0cbee16835a2b2e192768f149
Ricard Garcia <imricardgarcia@gmail.com>
2021-06-28 09:16:22 +0200

    feat: added link to all file/directory row

--- src/modules/directoryFiles.php
+++ src/modules/directoryFiles.php
@@ -17,28 +17,27 @@ foreach (scandir($target_dir) as $i) {
 
             // Creating the file block
             // echo "<a class='row file-item px-3 py-2 d-flex justify-content-center align-items-center' href=./modules/filePreview.php?filePath=" . $_SESSION["currentPath"] . "/" . $fileArray["name"] . "&fileName=" . $fileArray["name"] . ">";
-            echo "<div class= 'row file-item px-3 py-2 d-flex justify-content-center align-items-center'>";
+            // Adding a link for directories
+            if ($fileArray["type"] == "directory") {
+                echo "<a class='dir dir-link dir-child row file-item px-3 py-2 d-flex justify-content-center align-items-center' href=./modules/updatingPath.php?updatedPath=" . $_SESSION["currentPath"] . "/" . $fileArray["name"] . ">";
+            } else {
+                echo "<a class='dir dir-link dir-child row file-item px-3 py-2 d-flex justify-content-center align-items-center' href=./index.php?filePath=" . $_SESSION["currentPath"] . "/" . $fileArray["name"] . "&fileName=" . $fileArray["name"] . ">";
+            }
+            // echo "<a class='row file-item px-3 py-2 d-flex justify-content-center align-items-center'>";
             echo "<div class='row col col-6 p-0 icon-and-name d-flex justify-content-between align-items-center'>";
             echo "<p class='col col-2 file-text file-icon p-0 d-flex justify-content-center'>";
             echo "<i class='far fa-" . $fileArray["icon"] . "'></i>";
             echo "</p>";
-            // Adding a link for directories
-            if ($fileArray["type"] == "directory") {
-                echo "<p class='col col-10 file-text file-name'>";
-                echo "<a class='dir dir-link dir-child' href=./modules/updatingPath.php?updatedPath=" . $_SESSION["currentPath"] . "/" . $fileArray["name"] . ">";
-                echo $fileArray["name"];
-                echo "</a>";
-                echo "</p>";
-            } else {
-                echo "<p class='col col-10 file-text file-name'>" . $fileArray["name"] . "</p>";
-            }
+            echo "<p class='col col-10 file-text file-name'>";
+            echo $fileArray["name"];
+            echo "</p>";
             echo "</div>";
             // echo "<p class='col col-2 col file-text'>" . $fileArray["type"] . "</p>";
             // echo "<p class='file-text'>" . $fileArray["path"] . "</p>";
             echo "<p class='col col-2 file-text'>" . $fileArray["size"] . "</p>";
             echo "<p class='col col-2 file-text'>" . $fileArray["creation"] . "</p>";
             echo "<p class='col col-2 file-text'>" . $fileArray["modification"] . "</p>";
-            echo "</div>";
+            echo "</a>";
         }
     }
 };
