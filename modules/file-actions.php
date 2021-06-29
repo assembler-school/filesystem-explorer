bc67a680f38c869b1e42848d0aeca1b146633c70
김은영 <solaz0824@gmail.com>
2021-06-29 13:02:26 +0200

    Delete and Rename folders done

--- modules/file-actions.php
+++ modules/file-actions.php
@@ -1,35 +1,47 @@
-<?php
+ <?php
 
-function getAllFiles($dir, &$results = array())
-{
-    $files = scandir($dir);
-    foreach ($files as $key => $value) {
-        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
-        if (!is_dir($path)) {
-            $results[] = ['path' => realpath($path), 'name' => $value];
-        } else if ($value != "." && $value != "..") {
-            getAllFiles($path, $results);
-            $results[] = ['name' => $value];
+    function getAllFiles($dir, &$results = array())
+    {
+        $files = scandir($dir);
+        foreach ($files as $key => $value) {
+            $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+            if (!is_dir($path)) {
+                $results[] = ['path' => realpath($path), 'name' => $value];
+            } else if ($value != "." && $value != "..") {
+                getAllFiles($path, $results);
+                $results[] = ['path' => $path, 'name' => $value];
+            }
+        }
+        return $results;
+    }
+
+    $filesList = getAllFiles("../root");
+    $fileToDelete = $_POST["delete"];
+
+    function removeDirectory($path)
+    {
+        $files = glob($path . '/*');
+        foreach ($files as $file) {
+            is_dir($file) ? removeDirectory($file) : unlink($file);
+        }
+        rmdir($path);
+        return;
+    }
+    foreach ($filesList as $file) {
+        if (isset($fileToDelete)) {
+            if (is_file($file["path"]) && $file["name"] === $fileToDelete && file_exists($file["path"])) {
+                unlink($file["path"]);
+                header("Location: ../index.php?deleted=true");
+            } elseif (is_dir($file["path"]) && $file["name"] === $fileToDelete && file_exists($file["path"])) {
+                removeDirectory($file["path"]);
+                header("Location: ../index.php?deleted=true");
+            }
+        } elseif (isset($_POST["rename"]) && isset($_POST["newName"])) {
+            $fileName = explode(".", $file["name"])[0];
+            $rpName = str_replace($fileName, $_POST["newName"], $file["path"]);
+            if ($fileName === $_POST["rename"] && file_exists($file["path"])) {
+                rename($file["path"], $rpName);
+                header("Location: ../index.php?renamed=true");
+            }
         }
     }
-    return $results;
-}
-
-$filesList = getAllFiles("../root");
-$fileToDelete = $_POST["delete"];
-
-foreach ($filesList as $file) {
-    if (isset($fileToDelete)) {
-        if ($file["name"] === $fileToDelete && file_exists($file["path"])) {
-            unlink($file["path"]);
-            header("Location: ../index.php?deleted=true");
-        }
-    } elseif (isset($_POST["rename"]) && isset($_POST["newName"])) {
-        $fileName = explode(".", $file["name"])[0];
-        $rpName = str_replace($fileName, $_POST["newName"], $file["path"]);
-        if ($fileName === $_POST["rename"] && file_exists($file["path"])) {
-            rename($file["path"], $rpName);
-            header("Location: ../index.php?renamed=true");
-        }
-    }
-}
