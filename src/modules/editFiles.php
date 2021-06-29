12fa46a87771314ab3dee80cda7227d2fa71af7b
Ricard Garcia <imricardgarcia@gmail.com>
2021-06-29 17:42:10 +0200

    fix: solved rename bug

--- src/modules/editFiles.php
+++ src/modules/editFiles.php
@@ -5,10 +5,10 @@ $oldName = $_POST["oldFileName"];
 $newName = $_POST["fileName"];
 $pathToEdit =  dirname(getcwd()) . "/" . $_SESSION["currentPath"] . "/";
 
-// echo "Old name: " . $oldName . "<br>";
-// echo "New name: " . $newName . "<br>";
-// echo $pathToEdit . $oldName . "<br>";
-// echo $pathToEdit . $newName;
+echo "Old name: " . $oldName . "<br>";
+echo "New name: " . $newName . "<br>";
+echo $pathToEdit . $oldName . "<br>";
+echo $pathToEdit . $newName . "<br>";
 
 rename($pathToEdit . $oldName, $pathToEdit . $newName);
 
