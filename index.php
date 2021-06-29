bbfbb5f088d3f11502adf8b36e715e492348a119
Alberto <gs89alberto@gmail.com>
2021-06-29 09:48:17 +0200

    fontawesome added and icons

--- index.php
+++ index.php
@@ -17,13 +17,13 @@ include_once './src/newFolder.php';
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="./assets/styles/content.css">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
   <title>FileSystem Explorer</title>
 </head>
 
