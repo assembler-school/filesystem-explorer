6d4b4d1a79d23fdba939420ceda7e8df16b26870
Jonathan Cedeño <jony.cede@gmail.com>
2021-06-29 16:38:07 +0200

    feat: trash implemented and folder navigation fixes #22 fixes #23

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,3 +1,9 @@
 function showFiles(folderId){
   window.location = `${window.location.pathname}?folder-id=${folderId}`;
 }
+
+document.getElementById("trash-id").addEventListener("click", showFilesTrash);
+
+function showFilesTrash(){
+  window.location = `${window.location.pathname}?trash=true`;
+}
