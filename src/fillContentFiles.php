b6451e7155680cded9bb17a113e557ec1c995ac2
Alberto <gs89alberto@gmail.com>
2021-06-30 18:35:14 +0200

    search final version

--- src/fillContentFiles.php
+++ src/fillContentFiles.php
@@ -13,19 +13,23 @@ require_once('./src/fillContent.php');
 require_once('./src/fileIcons.php');
 
 $files = getFiles($_SESSION['currentPath']);
+if ($files) {
+  array_walk($files, function ($filePath) {
+    $fileIcon = getFileIcon($filePath);
+    $fileName = basename($filePath);
+    $shortFileName = strlen($fileName) > 22 ? substr($fileName, 0, 18) . '...' : $fileName;
+    $fileSize = human_filesize(filesize($filePath));
+    echo "<div class='file d-flex flex-column justify-content-end m-2 p-1' title='$fileName'>
+      <div class='file-top'>
+      <i class='d-flex justify-content-center fas fa-$fileIcon fa-5x'></i>
+      </div>
+      <div class='file-bottom'>
+        <p class='mx-1 my-0 d-flex justify-content-center'>$shortFileName</p>
+        <p class='mx-1 my-0 d-flex justify-content-end'>$fileSize</p>
+      </div>
+    </div>";
+  });
+} else {
+  echo "<img src='./assets/img/emptyFolder.png'>";
+}
 
-array_walk($files, function ($filePath) {
-  $fileIcon = getFileIcon($filePath);
-  $fileName = basename($filePath);
-  $shortFileName = strlen($fileName) > 22 ? substr($fileName, 0, 18) . '...' : $fileName;
-  $fileSize = human_filesize(filesize($filePath));
-  echo "<div class='file d-flex flex-column justify-content-end m-2 p-1' title='$fileName'>
-    <div class='file-top'>
-    <i class='d-flex justify-content-center fas fa-$fileIcon fa-5x'></i>
-    </div>
-    <div class='file-bottom'>
-      <p class='mx-1 my-0 d-flex justify-content-center'>$shortFileName</p>
-      <p class='mx-1 my-0 d-flex justify-content-end'>$fileSize</p>
-    </div>
-  </div>";
-});
