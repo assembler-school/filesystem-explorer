b0783309179e2fe8beaf1ea9d4989b4adc2c5587
MarcSola <marcsr03@gmail.com>
2021-06-30 11:53:31 +0200

    basic rename version without browsing done

--- includes/updateName.php
+++ includes/updateName.php
@@ -1,14 +1,27 @@
 <?php 
     if(isset($_POST['submitRename'])){
-        print_r($_POST);
+        
+        // Getting current dirPath
+        $dirPath = "../root/";
+        echo $dirPath;
+
+        // Getting all items in this dirPath
+        $dirItemList = scandir($dirPath);
+
+        // Searching item name to change and changing its name
+        $itemToChangeIndex = array_search($_POST['oldName'], $dirItemList);
+        rename($dirPath.$dirItemList[$itemToChangeIndex],$dirPath.$_POST['newName']);
+       
+        // Redirect to index.php
+       header("Location: ../index.php?renamesuccess");
     }
 
-    if (isset($_POST["rename"]) && isset($_POST["newName"])) {
-        $fileName = explode(".", $file["name"])[0];
-        $rpName = str_replace($fileName, $_POST["newName"], $file["path"]);
-        if ($fileName === $_POST["rename"]) {
-        rename($file["path"], $rpName);
-        header("Location: ../index.php?renamed=true");
-        }
-        }
+    // if (isset($_POST["rename"]) && isset($_POST["newName"])) {
+    //     $fileName = explode(".", $file["name"])[0];
+    //     $rpName = str_replace($fileName, $_POST["newName"], $file["path"]);
+    //     if ($fileName === $_POST["rename"]) {
+    //     rename($file["path"], $rpName);
+    //     header("Location: ../index.php?renamed=true");
+    //     }
+    //     }
 ?>
