4d7a015faaf7ee62240291d77fe8d27e567e57af
Enrique Hidalgo <koko.freelance@gmail.com>
2021-11-03 18:39:35 +0000

    done

--- includes/addfile.php
+++ includes/addfile.php
@@ -1,5 +1,30 @@
 <?php
+echo "<a href='javascript:history.back(1);'>Back</a>'";
+echo "<pre>";
+print_r($_FILES);
 
-$path ="/root/example.txt";
+$pathName= "../root";
+$fileName = $_FILES["addfile"]["name"];
+$fileType =$_FILES["addfile"]["type"];
+$fileTmp = $_FILES["addfile"]["tmp_name"];
+$fileSize = $_FILES["addfile"]["size"];
 
-echo dirname($path);
+if(!file_exists("../root")) {
+mkdir($pathName, 0777, true);
+chmod($pathName, 0777);
+}
+else if(file_exists("../root")) {
+
+  if(move_uploaded_file($fileTmp, "../root/" . $fileName)) {
+    echo "Uploaded file";
+  }else {
+    echo "File upload failed";
+  }
+
+} else {
+  if(move_uploaded_file($fileTmp, "../root/" . $fileName)) {
+    echo "Uploaded file";
+  } else {
+    echo "File upload failed";
+  }
+}
--- index.php
+++ index.php
@@ -41,7 +41,7 @@
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              <form method="POST" action="./includes/addfile.php">
+              <form method="POST" action="./includes/addfile.php" enctype="multipart/form-data">
                 <input type="file" name="addfile" />
             </div>
             <div class="modal-footer">
