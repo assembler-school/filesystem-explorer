23b6e1c2086709110e3006a1da7b78b3f57e3cb3
Josep Romero Ros <joseprrr@gmail.com>
2021-11-04 15:47:20 +0100

    pulling

---
+++ erase.php
@@ -0,0 +1,11 @@
+<?php
+    $eraseDir = $_GET["erase"];
+        print_r(pathinfo($eraseDir));
+    if(is_dir($eraseDir) == true){
+        rmdir($eraseDir);
+    }
+    else{
+        unlink($eraseDir);
+    }
+    header("Location:./index.php");  
+?>
