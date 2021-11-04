4bfd7fb9fc684124b322676b42127fb6cd174694
AntonioCopete <antcopjim@gmail.com>
2021-11-04 09:48:02 +0100

    upload file with folder navigation

--- sideBar.php
+++ sideBar.php
@@ -1,10 +1,17 @@
+<?php
+$directory = 'upload.php';
+if (isset($_GET["directory"])) {
+  $directory = "upload.php?directory=" . $_GET['directory'];
+}
+?>
+
 <section class="side__menu">
-    <form action="./createFolderForm.php" method="POST">
-        <input type="submit" value="Create folder">
-    </form>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-  Select file to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload File" name="submit">
-</form>
+  <form action="./createFolderForm.php" method="POST">
+    <input type="submit" value="Create folder">
+  </form>
+  <form <?php echo "action=$directory" ?> method="post" enctype="multipart/form-data">
+    Select file to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload File" name="submit">
+  </form>
 </section>
--- upload.php
+++ upload.php
@@ -1,14 +1,21 @@
+
 <?php
 
-$target_dir = "uploads/";
+$directory = "index.php";
+
+if (isset($_GET["directory"])) {
+  $directory = $_GET["directory"];
+}
+
+$target_dir = $_GET["directory"] ? $directory : "./root/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
+if (isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
+  if ($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
@@ -30,8 +37,10 @@ if ($_FILES["fileToUpload"]["size"] > 5000000) {
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
+if (
+  $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif"
+) {
   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
@@ -39,17 +48,13 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+  // if everything is ok, try to upload file
 } else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . "/" . $_FILES["fileToUpload"]["name"])) {
+    echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
 }
-?>
 
-
-
-
-
+header("Location:  index.php?directory=$directory");
