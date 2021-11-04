5236d3f5b33ec682815eb257318118ef034544c1
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-11-04 12:48:45 +0000

    feat-> rows (folder & size) added

--- resources/views/managerTable.php
+++ resources/views/managerTable.php
@@ -1,10 +1,12 @@
 <table id="table_id" class="table table-responsive table-striped table-bordered">
     <thead>
         <tr>
+            <th>Folder</th>
             <th>Name</th>
             <th>Type</th>
             <th>Create</th>
             <th>Modify</th>
+            <th>Size</th>
             <th>Actions</th>
         </tr>
     </thead>
@@ -13,16 +15,33 @@
         $arrayFile = glob('../../storage/*', GLOB_BRACE);
 
         foreach ($arrayFile as $file) {
-            $fileType = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+            $fileFolder = str_replace(dirname($file), '../../storage', '/');
             $fileName =  basename($file);
+            $fileType = strtolower(pathinfo($file, PATHINFO_EXTENSION));
             $fileCreate =  date("Y-m-d H:m:s", fileatime($file));
             $fileModify =  date("Y-m-d H:m:s", filemtime($file));
+
+            $fileSizeB = filesize($file);
+            if ($fileSizeB >= 0) {
+                $fileSize = $fileSizeB / 1024;
+                $format = "Kb";
+                if ($fileSize >= 1024) {
+                    $fileSize = $fileSize / 1024;
+                    $format = "Mb";
+                    if ($fileSize >= 1024) {
+                        $fileSize = $fileSize / 1024;
+                        $format = "Gb";
+                    }
+                }
+            }
             echo ' <tr>' .
+                ' <td> ' . $fileFolder . ' </td>' .
                 ' <td> ' . $fileName . ' </td> ' .
                 ' <td> ' . $fileType . ' </td>' .
                 ' <td> ' . $fileCreate . ' </td>' .
                 ' <td> ' . $fileModify . ' </td>' .
-                ' <td> ' . '$fileModify' . ' </td>' .
+                ' <td> ' . round($fileSize, 2) . $format . ' </td>' .
+                ' <td> ' . 'ACTIONS' . ' </td>' .
                 '</tr> ';
             # code...
         }
