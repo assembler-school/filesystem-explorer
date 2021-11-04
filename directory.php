568a16ba0271696c85c2b5de1d7650350a943d08
Josep Romero Ros <joseprrr@gmail.com>
2021-11-04 17:37:19 +0100

    styles

--- directory.php
+++ directory.php
@@ -1,14 +1,13 @@
 <div class="container d-flex align-items-center justify-content-center">
     <?php
     if (isset($_GET["directory"]) && $_GET["directory"] !== "" && $_GET["directory"] !== "root") {
-        print_r($_GET["directory"]);
+        // print_r($_GET["directory"]);
         $path = explode("/", $_GET["directory"]);
         array_pop($path);
         $directory = implode("/", $path);
     }
     ?>
     <?php echo isset($directory) ?  "<a href=index.php?directory=" . $directory . ">" : "<a href='index.php?directory=root'>" ?>
-        <button class="btn btn-info">Back</button> </a>
-        <button class="btn btn-info">></button>
-        
+        <button class="btn btn-info">^</button></a>
+        <?php echo isset($_GET["directory"])? $_GET["directory"] : "" ?>
 </div>
