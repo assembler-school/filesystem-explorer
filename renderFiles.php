32dad1e17925bb1a61ba94a1547f2064c1f560ab
AntonioCopete <antcopjim@gmail.com>
2021-11-05 11:59:07 +0100

    changing components

--- renderFiles.php
+++ renderFiles.php
@@ -1,23 +1,45 @@
 <?php
 
+function readDirectory($search, $folderPath)
+{
+    if (is_dir($folderPath)) {
+        if ($dh = opendir($folderPath)) {
+            while (($found = readdir($dh)) !== false) {
+                if ($found === "." || $found === "..") {
+                } else {
+
+                    if (str_contains($found, $search)) {
+                        $fullPath = $folderPath . '/' . $found;
+                        printDirectory($fullPath);
+                    }
+                    if (filetype($folderPath) == "dir") {
+                        // $folderPath = $folderPath . '/' . $folder;
+                        readDirectory($search, $folderPath . '/' . $found);
+                    }
+                }
+            }
+        }
+        closedir($dh);
+    }
+}
+
 function printDirectory($fullPath)
 {
     $modificationDate = date("d F Y H:i:s.", filemtime($fullPath));
     $creationDate = date("d F Y H:i:s.", filectime($fullPath));
+    $fileSize = filesize($fullPath) / 100 . " KB";
     $ext = pathinfo($fullPath, PATHINFO_EXTENSION);
 
     if (is_dir($fullPath)) {
-
-
         if (PHP_OS == "WINNT") {
             echo "
                 <div class='folder'>
                     <a class='folder' href=index.php?directory=$fullPath>
                         <p>$fullPath</p>
                     </a>
-                    <p>$creationDate</p>
-                    <p>$modificationDate</p>
-                    <p>$ext</p>
+                    <p>Created: $creationDate</p>
+                    <p>Modified: $modificationDate</p>
+                    <p>$fileSize</p>
                 </div>";
         } else {
             echo "
@@ -26,20 +48,20 @@ function printDirectory($fullPath)
                         <p>$fullPath</p>
                     </a>
                     <p>Unknown</p>
-                    <p>$modificationDate</p>
-                    <p>$ext</p>
+                    <p>Modified: $modificationDate</p>
+                    <p>$fileSize</p>
                 </div>";
         }
     } else {
-
         if (PHP_OS == "WINNT") {
             echo "
                 <div>
                     <div class='file'>
                         <p>$fullPath</p>
-                        <p>$creationDate</p>
-                        <p>$modificationDate</p>
+                        <p>Created: $creationDate</p>
+                        <p>Modified: $modificationDate</p>
                         <p>$ext</p>
+                        <p>$fileSize</p>
                     </div>
                 </div>";
         } else {
@@ -48,8 +70,9 @@ function printDirectory($fullPath)
                     <div class='file'>
                         <p>$fullPath</p>
                         <p>Unknown</p>
-                        <p>$modificationDate</p>
+                        <p>Modified: $modificationDate</p>
                         <p>$ext</p>
+                        <p>$fileSize</p>
                     </div>
                 </div>";
         }
@@ -69,13 +92,6 @@ if (!isset($_GET["search"])) {
                 if ($file === "." || $file === "..") {
                 } else {
                     $fullPath = "$directory/$file";
-                    // if (filetype("$directory/$file") == "dir") {
-                    //     echo "<div><a class='folder' href=index.php?directory=" . $directory . "/" . $file . ">$file</a>
-
-                    //     <a href=erase.php?erase=$directory/$file><button>x</button></a></div>";
-                    // } else {
-                    //     echo "<div><a class='file' href=index.php?directory=" . $directory . "/" . $file . ">$file</a><a href=erase.php?erase=$directory/$file><button>x</button></a></div>";
-                    // }
                     printDirectory($fullPath);
                 }
             }
@@ -83,36 +99,7 @@ if (!isset($_GET["search"])) {
         }
     }
 } else {
-    function readDirectory($search, $folderPath)
-    {
-        if (is_dir($folderPath)) {
-            if ($dh = opendir($folderPath)) {
-                while (($found = readdir($dh)) !== false) {
-                    if ($found === "." || $found === "..") {
-                    } else {
-
-                        if (str_contains($found, $search)) {
-                            $fullPath = $folderPath . '/' . $found;
-                            printDirectory($fullPath);
-                        }
-                        if (filetype($folderPath) == "dir") {
-                            // $folderPath = $folderPath . '/' . $folder;
-                            readDirectory($search, $folderPath . '/' . $found);
-                        }
-                    }
-                }
-            }
-            closedir($dh);
-        } else {
-            // if (is_file($folderPath)) {
-            //     echo "<div>$folderPath</div>";
-            // }
-        }
-    }
-    if (isset($_GET["search"])) {
-        $search = $_GET["search"];
-        $folderPath = "root";
-
-        readDirectory($search, $folderPath);
-    }
+    $search = $_GET["search"];
+    $folderPath = "root";
+    readDirectory($search, $folderPath);
 }
