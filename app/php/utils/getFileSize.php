6b275731f83c81cb088e3967a1c331d82a0a11af
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-11-08 11:37:46 +0000

    fix->getFileSize

--- app/php/utils/getFileSize.php
+++ app/php/utils/getFileSize.php
@@ -2,13 +2,15 @@
 function getFileSize($file)
 {
     $fileSize = filesize($file);
-    if ($fileSize >= 0) {
+    if ($fileSize > 0) {
         $i = 0;
         $format = ["Kb", "Mb", "Gb"];
         while ($fileSize >= 1024) {
             $fileSize = $fileSize / 1024;
             $i++;
         }
+        return round($fileSize, 2) . $format[$i - 1];
+    } else {
+        return "0kb";
     }
-    return round($fileSize, 2) . $format[$i - 1];
 }
