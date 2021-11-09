b628a8947182547f076b0904df0eddfeb8b4d63d
AntonioCopetem <antcopjim@gmail.com>
2021-11-09 10:37:04 +0100

    fixing form

--- components/createFolderForm.php
+++ components/createFolderForm.php
@@ -9,7 +9,7 @@ if (isset($_POST["folder"])) {
 }
 if(!(isset($_GET["search"])||isset($_GET["edit"]) )) {
     echo "<div class='d-flex my-2'>
-    <form class='d-flex w-100' action=' method='POST'>
+    <form class='d-flex w-100' action='' method='POST'>
         <input class='form-control' type='text' placeholder='Folder name' name='folder'>
         <input class='btn btn-outline-secondary' type='submit' value='Create folder'>
     </form>
