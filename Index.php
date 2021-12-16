5c719eb917b611700513dc5a32249281014b9c3d
Adrián Serrano <adrianno_3@hotmail.com>
2021-12-16 18:11:36 +0100

    Index structure nav and row

--- Index.php
+++ Index.php
@@ -4,9 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" defer></script>
+    <script src="./assets/js/script.js" defer></script>
     <title>Files System Explorer</title>
 </head>
 <body>
+    <div class="jumbotron">
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand">Our Cloud</a>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+
+        <nav class="navbar navbar-light bg-light">
+            <form class="container-fluid justify-content-start">
+                <button class="btn btn-sm btn-outline-secondary me-2" type="button">New</button>
+                <button class="btn btn-sm btn-outline-secondary me-2" type="button">Unload</button>
+                <button class="btn btn-sm btn-outline-secondary me-2" type="button">Download</button>
+                <button class="btn btn-sm btn-outline-secondary me-2" type="button">Move to</button>
+                <button class="btn btn-sm btn-outline-secondary me-2" type="button">Copy to</button>
+                <button class="btn btn-sm btn-outline-secondary me-2" type="button">Rename</button>
+            </form>
+        </nav>
+    </div>
+<div class="row">
+    <div class="col-4 primary" >Hola</div>
+    <div class="col-4 secondary">Adios</div>
+    <div class="col-4 primary">Hola</div>
+</div>
 
 </body>
 </html>
