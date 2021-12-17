5d6c34736893e6f79fb8f9ee1f2c708049f12b97
Bertinoti <jeffersonbertinoti@gmail.com>
2021-12-17 11:30:15 +0100

    start project

---
+++ File System Project/index.php
@@ -0,0 +1,14 @@
+<?php
+    include 'modules/htmlHeader.php'
+?>
+
+<body>
+    <nav>
+        
+        <?php include 'modules/createButton.php' ?>
+
+    </nav>
+    
+    
+</body>
+</html>
---
+++ File System Project/modules/htmlHeader.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rocket File System </title>
+
+    <!--Todo Links CSS -->
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--Todo  Links CDN-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    <!--Todo Scripts JavaScript -->
+    <script src="assets/scripts/mainScript.js"></script>
