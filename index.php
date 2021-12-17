95adab560c9fc5a5b3cb9672b11370876fa7f487
Luis Molina <luismateo2416@gmail.com>
2021-12-17 01:31:03 +0100

    main section first example, url nav bar created

--- index.php
+++ index.php
@@ -6,9 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
+    <!-- jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <!-- bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/index.css">
+    <!-- font-awesome -->
+    <script src="https://kit.fontawesome.com/63f29c9463.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -18,19 +22,34 @@
 
         </figure>
     </header>
-    <nav class="url">
-
-    </nav>
     <!-- Here commes the directories -->
     <aside>
 
     </aside>
-    <!-- Main has to be a grid or flexbox responsive with cols and rows of bootstrap -->
-    <main>
-        <!-- Cada section sera un fichero diferente que carga -->
-        <section>
+    <!-- navigation url -->
+    <nav class="url">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Library</li>
+            <i class="fas fa-info-circle fa-2x" id="infoCircle"></i>
+        </ol>
+    </nav>
 
-        </section>
+    <!-- Main has to be a grid or flexbox responsive with cols and rows of bootstrap -->
+    <main class="container">
+        <!-- Cada section sera un fichero diferente que carga -->
+        <article class="row">
+            <section class="col-4">
+                <div class="col d-flex flex-column">
+                    <img src="./assets/img/test.jpg" alt="photo">
+                    <div class="infoCard">
+                        <img src="./assets/img/img-icon.png" alt="img-icon" width="50">
+                        <p class=" fileName">File.png</p>
+                    </div>
+                </div>
+            </section>
+
+        </article>
     </main>
 </body>
 
