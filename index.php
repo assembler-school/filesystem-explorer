844de527f2a49336e97f6e09261825a1cf91fe31
Luis Molina <luismateo2416@gmail.com>
2021-12-20 10:41:14 +0100

    added bg color in main html structure

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <title>Index</title>
     <!-- jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -73,38 +72,38 @@
                 </div>
             </div>
         </div>
-        <section class="mainC">
-            <!-- Main has to be a grid or flexbox responsive with cols and rows of bootstrap -->
-            <main class="containerR">
-                <!-- Cada section sera un fichero diferente que carga -->
-                <article class="row">
-                    <section class="col-4">
-                        <div class="col d-flex flex-column">
-                            <img src="./assets/img/test.jpg" alt="photo" width="100%">
-                            <div class="infoCard">
-                                <img src="./assets/img/img-icon.png" alt="img-icon" width="50px">
-                                <p class=" fileName">File.png</p>
-                            </div>
+
+        <!-- Main has to be a grid or flexbox responsive with cols and rows of bootstrap -->
+        <main class="container">
+            <!-- Cada section sera un fichero diferente que carga -->
+            <article class="row">
+                <section class="col-4">
+                    <div class="col d-flex flex-column">
+                        <img src="./assets/img/test.jpg" alt="photo" width="100%">
+                        <div class="infoCard">
+                            <img src="./assets/img/img-icon.png" alt="img-icon" width="50px">
+                            <p class=" fileName">File.png</p>
                         </div>
-                        <div class="col d-flex flex-column">
-                            <img src="./assets/img/test.jpg" alt="photo" width="100%">
-                            <div class="infoCard">
-                                <img src="./assets/img/img-icon.png" alt="img-icon" width="50px">
-                                <p class=" fileName">File.png</p>
-                            </div>
+                    </div>
+                    <div class="col d-flex flex-column">
+                        <img src="./assets/img/test.jpg" alt="photo" width="100%">
+                        <div class="infoCard">
+                            <img src="./assets/img/img-icon.png" alt="img-icon" width="50px">
+                            <p class=" fileName">File.png</p>
                         </div>
-                        <div class="col d-flex flex-column">
-                            <video src="https://codingyaar.com/wp-content/uploads/video-in-bootstrap-card.mp4" controls width="100%"></video>
-                            <div class="infoCard">
-                                <i class="fa fa-video-camera" width="50px"></i>
-                                <p class=" fileName">Video</p>
-                            </div>
+                    </div>
+                    <div class="col d-flex flex-column">
+                        <video src="https://codingyaar.com/wp-content/uploads/video-in-bootstrap-card.mp4" controls width="100%"></video>
+                        <div class="infoCard">
+                            <i class="fa fa-video-camera" width="50px"></i>
+                            <p class=" fileName">Video</p>
                         </div>
-                    </section>
-                </article>
-            </main>
-        </section>
+                    </div>
+                </section>
+            </article>
+        </main>
     </section>
+
 </body>
 
 </html>
