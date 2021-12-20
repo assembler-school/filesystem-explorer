3061c0cdd68cfc8bd610f2e2e71a11cf10fd72c3
Carla <91063081+cicu90@users.noreply.github.com>
2021-12-20 11:30:10 +0000

    create file

--- modules/create.php
+++ modules/create.php
@@ -3,9 +3,11 @@
 if(isset($_POST['create_file']))
 {
   $file_name=$_POST['file_name'];
-  $newFile =fopen("root/file_name", "w+") or die("Archivo not found");
+  $newFile =fopen("root/file_name", "w") or die("File not found");
+  $txt = "New file";
   fwrite($newFile, $txt);
  fclose($newFile);
 }
+
 
 ?>
