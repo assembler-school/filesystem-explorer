9ab636642dec30734312878bda45b639c9125cb0
Bertinoti <jeffersonbertinoti@gmail.com>
2021-12-20 13:11:33 +0100

    main page

--- File System Project/index.php
+++ File System Project/index.php
@@ -7,51 +7,25 @@ include 'modules/createModal.php';
 ?>
 
 <body>
-    <?php
+    <div class="row">
+        <aside class="col-3">
+            <?php require 'showDir.php'  ?>
+        </aside>
+        <section class="col-6">
+            <img src="./Root/assembler.jpg" alt="" width="100%">
+        </section>
+        <aside class="col-3">
+        <img src="./Root/1630658839121.gif" alt="" width="100%">
+        </aside>
+    </div>
 
-    $path = "./root";
-    $dir = new DirectoryIterator($path);
-    foreach ($dir as $fileinfo) {
-        if ($fileinfo->isDir() && !$fileinfo->isDot()) {
-            $credentials = dirname($fileinfo);
-            $pathofFile =  $fileinfo->getFileInfo();
-            echo "<a href='$pathofFile'> <i class='fas fa-folder fa-10x'></i></a>";
-              // print_r($credentials);
-              echo $fileinfo->getFileInfo().'<br>';
-              echo $fileinfo->getFilename().'<br>';
-              echo date('F d Y H:i:s', filemtime($pathofFile)) . '<br>';
-              echo bytesToHuman(folderSize($pathofFile));
-          }
-      }
-      foreach ($dir as $fileF){
-          if ($fileF -> isfile() ){
-              echo $fileF."<br>";
-            print $fileF;
-          }
-      }
-     
-?>
-<form action="./createfolder.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="Fileimage" id="fileofimage">
-    <input type="submit" value="Upload a file" name="buttclick">
-</form>
-          
-    <?= isset($_GET["error"]) ? "The name already exist" : "";
+    <form action="./createfolder.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="Fileimage" id="fileofimage">
+        <input type="submit" value="Upload a file" name="buttclick">
+    </form>
 
-    function bytesToHuman($bytes){
-        $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
-        for ($i = 0; $bytes > 1024; $i++) $bytes /= 1024;
-        return round($bytes, 3) . ' ' . $units[$i];
-    };
+    <?= isset($_GET["error"]) ? "The name already exist" : ""; ?>
 
-    function folderSize($dir){
-        $size = 0;
-        foreach (glob(rtrim($dir, '/') . '/*', GLOB_NOSORT) as $each) {
-            $size += is_file($each) ? filesize($each) : folderSize($each);
-        }
-        return $size;
-    };
-    ?>
 </body>
 
 </html>
--- File System Project/showDir.php
+++ File System Project/showDir.php
@@ -1,14 +1,38 @@
 <?php
-$path = "./root";
-$dir = new DirectoryIterator($path);
-foreach ($dir as $fileinfo) {
-    if ($fileinfo->isDir() && !$fileinfo->isDot()) {
-        $credentials = dirname($fileinfo);
-        $pathofFile =  $fileinfo->getFileInfo();
-        
-        //echo "<a href='$pathofFile'> <i class='fas fa-folder fa-10x'></i></a>";
-        //file root
-        //echo $fileinfo->getFileInfo();
-       // echo $fileinfo->getFilename() . '<br>';
-    }
-}
+    $path = "./root";
+    $dir = new DirectoryIterator($path);
+    foreach ($dir as $fileinfo) {
+        if ($fileinfo->isDir() && !$fileinfo->isDot()) {
+            $credentials = dirname($fileinfo);
+            $pathofFile =  $fileinfo->getFileInfo();
+            echo "<a href='$pathofFile'> <i class='fas fa-folder fa-10x'></i></a>";
+            echo $fileinfo->getFilename() . '<br>';
+            echo date('F d Y H:i:s', filemtime($pathofFile)) . '<br>';
+            echo bytesToHuman(folderSize($pathofFile));
+        }
+    };
+
+    foreach ($dir as $fileF) {
+        if ($fileF->isfile()) {
+            echo $fileF . "<br>";
+            print $fileF;
+        }
+    };
+
+    //todo Convert bites to readable size
+    function bytesToHuman($bytes)
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+        for ($i = 0; $bytes > 1024; $i++) $bytes /= 1024;
+        return round($bytes, 3) . ' ' . $units[$i];
+    };
+
+    //todo return the size of folder or file
+    function folderSize($dir)
+    {
+        $size = 0;
+        foreach (glob(rtrim($dir, '/') . '/*', GLOB_NOSORT) as $each) {
+            $size += is_file($each) ? filesize($each) : folderSize($each);
+        }
+        return $size;
+    };
