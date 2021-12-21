791116b5ab5c87ebbc1c742d600d57c3574af6b3
RoyerMateu <rogermateu2001@gmail.com>
2021-12-21 11:01:30 +0100

    show trash icon

--- modules/index.js
+++ modules/index.js
@@ -9,3 +9,17 @@ $(document).ready(function(){
 $("#btnCreate").on("click", function(){
 console.log("funciona");
 })
+
+//show trash icon
+var files = document.getElementsByClassName("sub-item")
+var svgTrash = document.getElementsByClassName("trash-icon")
+for (let i = 0; i < files.length; i++) {
+  files[i].addEventListener("mouseover", showTrash)
+  files[i].addEventListener("mouseout", notShowTrash)
+  function showTrash() {
+    svgTrash[i].style.display = 'block'
+}
+function notShowTrash(){
+  svgTrash[i].style.display = 'none'
+}
+}
