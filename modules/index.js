1ecb8e5d52acd0e39a61dc6a6f843cf939138f17
RoyerMateu <rogermateu2001@gmail.com>
2021-12-21 17:23:22 +0100

    Open modals + trash icon fix

--- modules/index.js
+++ modules/index.js
@@ -9,20 +9,3 @@ $(document).ready(function () {
 $("#btnCreate").on("click", function () {
   console.log("funciona");
 })
-
-
-//show trash icon
-var files = document.getElementsByClassName("sub-item")
-var svgTrash = document.getElementsByClassName("trash-icon")
-for (let i = 0; i < files.length; i++) {
-  files[i].addEventListener("mouseover", showTrash)
-  files[i].addEventListener("mouseout", notShowTrash)
-  function showTrash() {
-    svgTrash[i].style.display = 'block'
-}
-function notShowTrash(){
-  svgTrash[i].style.display = 'none'
-}
-}
-
-
