8d9f7bfd89d6e43abee39f449a3f4d1880560219
JSSatorres <juansataz@gmail.com>
2021-12-21 15:31:39 +0100

    unfinish rename folder

--- modules/renameFile.php
+++ modules/renameFile.php
@@ -1,12 +1,24 @@
 <?php
+if(isset( $_GET["fileName"])){
+    $root = $_GET["root"];
+    $fileName = $_GET["fileName"];
+    $inputNewName= $_POST["inputNewName"];
+    $oldName = ".".$root."/".$fileName;
+    $extension = pathinfo($oldName, PATHINFO_EXTENSION);
+    $newName = ".".$root."/".$inputNewName.".".$extension;
+    rename($oldName,$newName);
+}
+else if(isset($_GET["root"])){
 $root = $_GET["root"];
-$fileName = $_GET["fileName"];
-$inputNewName= $_POST["inputNewName"];
-$oldName = ".".$root."/".$fileName;
-$extension = pathinfo($oldName, PATHINFO_EXTENSION);
-echo $extension;
-$newName = ".".$root."/".$inputNewName.".".$extension;
-rename($oldName,$newName);
+$positionBar= strrpos($root,"/")+1;
+$stringFolderName = substr($root,0, $positionBar);
 
-header("Location:../index.php?root=$root&fileName=$newName");
+$oldNameFolder = ".".$root;
+$inputNewFolderName= $_POST["inputNewName"];
+$newFolderName =".".$stringFolderName."/".$inputNewFolderName;
+rename($oldNameFolder,$newFolderName);
+}
+
+
+// header("Location:../index.php?root=$root&fileName=$newName");
 
