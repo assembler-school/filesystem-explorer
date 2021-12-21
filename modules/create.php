e7c526b4937628f420edc0fa216e2a49a2401198
Luis Molina <luismateo2416@gmail.com>
2021-12-21 16:29:54 +0100

    create folder fix

--- modules/create.php
+++ modules/create.php
@@ -2,15 +2,16 @@
 if ($_POST["path"] != "") {
 
   $my_dir = './../' . $_POST["path"] . '/';
-  echo $my_dir;
+  $url = '?path=' . $_POST["path"];
 } else {
   $my_dir = "./../root/";
-  echo $my_dir;
+  $url = "";
 }
 $my_dir .= $_POST["file_name"];
 if (!is_dir($my_dir)) {
   mkdir($my_dir);
   echo "Se ha creado el directorio $my_dir";
+  header('location:./../index.php' . $url);
 } else {
   echo "el directorio $my_dir ya existe! No lo vamos a crear de nuevo";
 }
