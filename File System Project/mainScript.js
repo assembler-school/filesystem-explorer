623875c6b6d914b065e642877b03455804b1f354
Bertinoti <jeffersonbertinoti@gmail.com>
2021-12-22 11:54:38 +0100

    cleaning

--- File System Project/mainScript.js
+++ File System Project/mainScript.js
@@ -1,42 +1,16 @@
 function obtenerdatos(elem){
-    console.log("kjaskjdna")
-    console.log(elem)
-    // const url= elem;
-    // console.log(elem)
     fetch(elem)
     .then(respuesta => {
         return respuesta
     }).then(datos => {
-        console.log(datos.url)
         appendfile(datos.url)
     })
-    // console.log(linkfile);
 
 }
-
 
 function appendfile(elem){
     asidefile= document.getElementById('asidefile');
     console.log(asidefile);
-    // asidefile.innerHTML=""
     content=`<iframe src="${elem}" title="W3Schools Free Online Web Tutorials" width="640" height="480"></iframe>`
     asidefile.innerHTML = content;
 };
-
-// function printhere(elem){
-//     //console.log(elem)
-//     var retornar = 
-    
-    
-//     // $.ajax({
-//     //     type: "GET",
-//     //     url: "./tryUno.php",
-//     //     data: { functionname: "truOne", arguments: [elem] },
-//     //     success: function (response) {
-//     //         console.log('aklsjdblfkjabs')
-//     //     }
-//     // });
-//     console.log(retornar);
-//     return retornar;
-
-// }
