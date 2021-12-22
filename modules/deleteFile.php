263f410444ec4c867c141aa535df0d613701bfc5
JSSatorres <juansataz@gmail.com>
2021-12-22 18:34:14 +0100

    try delete nested folder

--- modules/deleteFile.php
+++ modules/deleteFile.php
@@ -7,14 +7,61 @@ if (isset($_GET["fileName"])){
     header("Location:../index.php?root=$root");
   }
   else if(isset($_GET["root"])) {
-    $root=$_GET["root"];
+    $root=".".$_GET["root"];
         //try{
-        rmdir(".".$root);
-            if(is_dir(".".$root)){
-                //throw new Exception("dont delete");
-                header("Location:../index.php?error='FolderNotDeleted'");
-            }
-            else {
-                header("Location:../index.php?error='FolderDeleted'");
-            }
+
+ deleteRecursive($root);
+
+
+
+
+
+
+        // rmdir(".".$root);
+        //     if(is_dir(".".$root)){
+        //         //throw new Exception("dont delete");
+        //         header("Location:../index.php?error='FolderNotDeleted'");
+        //     }
+        //     else {
+        //         header("Location:../index.php?error='FolderDeleted'");
+        //     }
         }
+
+
+function deleteRecursive($root){
+   if ( is_dir( $root));
+   {
+        $gestor = opendir($root);
+
+        // $directorios = scandir($root);
+        // print_r( $directorios);
+        // print_r(count($directorios));
+    // if (count($directorios)<=2)
+    //     {
+    //         rmdir($root); 
+    //     }
+
+        while (($element = readdir($gestor)) !== false){
+            $ruta_completa= "$root/$element";
+            if ($element === '.' || $element === '..') {
+                continue;
+            }
+
+            if(is_dir($ruta_completa)){
+                $directoriosChild = scandir($ruta_completa);
+                if(count($directoriosChild) <=2){
+                    rmdir($ruta_completa);
+                    echo "si borro";
+                    
+                }
+            deleteRecursive($ruta_completa);
+            }
+
+            else{
+                unlink($ruta_completa);
+            }
+
+        }
+        closedir($gestor);
+}
+}
