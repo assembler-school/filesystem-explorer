d959679a286cb903a7a7abe32149adb8d1858ec2
Bertinoti <jeffersonbertinoti@gmail.com>
2021-12-22 09:05:42 +0100

    fix nav bar

--- File System Project/modules/navBar.php
+++ File System Project/modules/navBar.php
@@ -1,23 +1,27 @@
 <nav class="navbar navbar-dark navbar-expand-sm  justify-content-between" style="background-color: #1c2331;">
 
-    <div class="col-2 logo">
+    <div class="col-2 logo col-sm-3">
         <a href="#" class="navbar-brand font-weight-bold"> <i class="fas fa-fighter-jet"></i> <i class="bi bi-alarm-fill"></i>
 Jet File System </a>
     </div>
-    <div class="col-5 ">
-        <div class="input-group">
-            <input type="search" id="buscar" class="form-control border-right-0 border" placeholder="Buscar...">
-            <div class="input-group-append">
-                <button type="button" class="btn btn-secondary"><i class="fas fa-search"></i> </button>
+    <div class="container">
+        <div class="col-5 col-sm-8">
+            <div class="input-group">
+                <input type="search" id="buscar" class="form-control border-right-0 border" placeholder="Buscar...">
+                <div class="input-group-append">
+                    <button type="button" class="btn btn-secondary"><i class="fas fa-search"></i> </button>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-offset-2 col-2 ">
-        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalForm">
-            Create File/ Folder
-        </button>
-        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalFormNew">
-            Upload File/ Folder
-        </button>
+        <div class="col-2  col-sm-6">
+            <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalForm">
+                Create File/ Folder
+            </button>
+        </div>
+        <div class="col-2 col-sm-6">
+            <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalFormNew">
+                Upload File/ Folder
+            </button>
+        </div>
     </div>
 </nav>
