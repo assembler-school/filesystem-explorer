b6753127e1cf37d907ab076c65d56c20eced892e
Erick Ayala <erickayalrias@gmail.com>
2021-12-22 20:16:00 +0100

    search

--- File System Project/index.php
+++ File System Project/index.php
@@ -43,7 +43,37 @@ include 'modules/createModal.php';
                 </thead>
                 <tbody>
                     <?php sectionFilesTable();
-                     ?>
+                    ?>
+                    <?php 
+if (isset($_POST['submitBuscar'])) {
+    $searchbar= $_POST['buscar'];
+    $path = "./root";
+    $newDir = new DirectoryIterator($path);
+    foreach ($newDir as $fileinfo) {
+        foreach ($fileinfo as $newroute) {
+            $newPatha = "./root/$newroute";
+            $pathtolook = new DirectoryIterator($newPatha);
+            foreach ($pathtolook as $rightPath) {
+                // $extension = $rightPath->getExtension();
+                // $infofile = $rightPath->getFileInfo();
+                $fileName = $rightPath->getFilename();
+                if($fileName ==$searchbar){
+                    // echo "entra aqui?";
+                    // fillTable("","","","", "");
+                    $extensiona = $rightPath->getExtension();
+                    $infofilea = $rightPath->getFileInfo();
+                $fileNamea = $rightPath->getFilename();
+                $rutanueva= folderSize($infofilea);
+                $fileSizea = bytesToHuman($rutanueva);
+                $lastUpdatea= date('F d Y H:i:s', filemtime($infofilea));
+                 fillTable($extensiona, $infofilea, $fileNamea, $fileSizea, $lastUpdatea);
+            }else{
+                echo "ruta mala";
+            }
+        }
+    }
+}
+}       ?>
                 </tbody>
             </table>
         </section>
---
+++ File System Project/search.php
@@ -0,0 +1,38 @@
+<?php
+ require_once("showDir.php");
+
+if (isset($_POST['submitBuscar'])) {
+    $searchbar= $_POST['buscar'];
+    // header("location: ./index.php");
+    // searchFile($searchbar);
+    $path = "./root";
+    $newDir = new DirectoryIterator($path);
+    foreach ($newDir as $fileinfo) {
+        foreach ($fileinfo as $newroute) {
+            $newPatha = "./root/$newroute";
+            $pathtolook = new DirectoryIterator($newPatha);
+            foreach ($pathtolook as $rightPath) {
+                // $extension = $rightPath->getExtension();
+                // $infofile = $rightPath->getFileInfo();
+                $fileName = $rightPath->getFilename();
+                if($fileName ==$searchbar){
+                    // echo "entra aqui?";
+                    // fillTable("","","","", "");
+                    $extensiona = $rightPath->getExtension();
+                    $infofilea = $rightPath->getFileInfo();
+                $fileNamea = $rightPath->getFilename();
+                $rutanueva= folderSize($infofilea);
+                $fileSizea = bytesToHuman($rutanueva);
+                $lastUpdatea= date('F d Y H:i:s', filemtime($infofilea));
+                 fillTable($extensiona, $infofilea, $fileNamea, $fileSizea, $lastUpdatea);
+            }
+            header("location: ./index.php?julio");
+            // echo $fileName."<br>";
+        }
+    }
+}
+}
+echo "akjsd";
+
+// global $prt;
+// echo $prt;
