37b3eece4ffe5d9fd17dbcdb4f62f3faaefd2a53
juanfco86 <juanfco86@gmail.com>
2022-09-07 16:48:00 +0200

    Header

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/54141fca8b.js" crossorigin="anonymous"></script>
+  <link href="./assets/css/index.css" rel="stylesheet">
+  <title>File System</title>
+
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+      <img src="./assets/img/logo.svg" alt="Logotipo" height="45px">
+    </div>
+    <nav>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2 search-bar" type="search" placeholder="Search" aria-label="Search">
+        <input class="btn btn-outline-success" type="submit" value="Search">
+      </form>
+
+      <div class="btns-file-action">
+        <div class="upload-file">
+          <span>Upload</span>
+          <i class="fa-solid fa-arrow-up-from-bracket"></i>
+        </div>
+
+        <div class="new-folder">
+          <span>New Folder</span>
+          <i class="fa-solid fa-folder-plus"></i>
+        </div>
+        <div class="move-file">
+          <span>Move File</span>
+          <i class="fa-solid fa-file-export"></i>
+        </div>
+        <div class="delete-file">
+          <span>Delete</span>
+          <i class="fa-solid fa-trash"></i>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <aside>
+    Ho
+  </aside>
+
+  <main>
+    Hola
+  </main>
+
+</body>
+
+</html>
