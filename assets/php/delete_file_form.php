bf845a47168c9c3bf1bedcbbb8c32d45a37d7c71
AlejandroLHC <alejandrolhc@gmail.com>
2022-09-09 11:11:30 +0200

    fix trouble previous commit (delete file v.2)

--- assets/php/delete_file_form.php
+++ assets/php/delete_file_form.php
@@ -12,14 +12,7 @@
                     if(!is_dir($path.'/'.$files[$i])) {
                         echo '<option value="../.'.$path.'/'.$files[$i].'">'.$files[$i].'</option>';
                     } else {
-<<<<<<< HEAD
                         echo '<option value="(F)../.'.$path.'/'.$files[$i].'">Folder: '.$files[$i].' </option>';
-=======
-<<<<<<< HEAD
-                        echo '<option value="(F)../.'.$path.'/'.$files[$i].'">Folder: '.$files[$i].' </option>';
-=======
->>>>>>> ee5e19a2468c803f6fb5d95f940d86cdcd485d9c
->>>>>>> c4add03c8b5c7566dd24ae7abf36711bc0f25988
                         deleteOptions($path.'/'.$files[$i]);
                     }
                 }
