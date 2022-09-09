05919d5b2dcea6a6235a8cb5ebf885d2c59c659a
AlejandroLHC <alejandrolhc@gmail.com>
2022-09-09 10:56:11 +0200

    delete file v.1

---
+++ assets/php/delete_file.php
@@ -0,0 +1,22 @@
+<?php 
+    deleteFile();
+
+    function deleteFile(){
+        $filepath = $_POST['file'];
+        if (substr($filepath, 0, 3) == '(F)') {
+            $dirpath = substr($filepath, 3);
+            if (rmdir($dirpath)) {
+                header ('Location: ../../index.php');
+            } else {
+                echo "Sorry, there was a problem deleting the directory.";
+                echo '<p><a href="../../index.php">Back</a></p>';
+            } 
+        } else {
+            if (unlink($filepath)) {
+                header ('Location: ../../index.php');
+            } else {
+                echo "Sorry, there was a problem deleting the file.";
+                echo '<p><a href="../../index.php">Back</a></p>';
+            }
+        }
+    }
