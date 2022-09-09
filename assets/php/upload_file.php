b2a920d20c23143dff648e0ebe973fc9b16f3192
AlejandroLHC <alejandrolhc@gmail.com>
2022-09-09 10:59:45 +0200

    delete file v.1

--- assets/php/upload_file.php
+++ assets/php/upload_file.php
@@ -7,7 +7,6 @@
         $path = $_FILES['userfile']['name'];
         $ext = pathinfo($path, PATHINFO_EXTENSION);
         $target_file = $target_dir . $filename . '.' . $ext;
-        print_r($target_dir . $filename . '.' . $ext);
         
         if (!file_exists($target_file)) {
             if (move_uploaded_file($_FILES['userfile']['tmp_name'], $target_file)) {
