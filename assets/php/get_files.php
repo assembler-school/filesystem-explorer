18029effbcf3d4f2c823c107303dbc503da75a7a
AlejandroLHC <alejandrolhc@gmail.com>
2022-09-13 13:48:32 +0200

    clean get_files php doc

--- assets/php/get_files.php
+++ assets/php/get_files.php
@@ -11,10 +11,11 @@
                     }
                 }
                 for ($i = 0; $i < count($files); $i++) {
-                    if(is_dir($path.'/'.$files[$i])) {
-                        echo '<a href="./'.$files[$i].'.php"><li>'. $files[$i] . ' <i class="fa-solid fa-caret-right"></i></li></a>';
-                        getFolders($path.'/'.$files[$i]);
-                        newIndex($files[$i], $path.'/'.$files[$i], $_SESSION['page']);
+                    $newPath = $path.'/'.$files[$i];
+                    if(is_dir($newPath)) {
+                        echo '<a href="./'.$files[$i].'.php?name="'.$newPath.'"><li>'. $files[$i] . ' <i class="fa-solid fa-caret-right"></i></li></a>';
+                        getFolders($newPath);
+                        newIndex($files[$i], $newPath, $_SESSION['page']);
                     }
                 }
                 echo '</ul>';
@@ -40,9 +41,10 @@
                         echo '<h4 class="files-path">My Files'. substr($path, 6). '</h4>';
                         echo '<div class="files-container">';
                         for ($i = 0; $i < count($files); $i++) {
-                            if(!is_dir($path.'/'.$files[$i])) {
-                                $infoFile = pathinfo($path.'/'.$files[$i]);
-                                echo '<a class="file-link" target="_blank" href="'.$path.'/'.$files[$i].'"><div class="found-file">';
+                            $newPath = $path.'/'.$files[$i];
+                            if(!is_dir($newPath)) {
+                                $infoFile = pathinfo($newPath);
+                                echo '<a class="file-link" target="_blank" href="'.$newPath.'"><div class="found-file">';
                                 echo '<div class="icon-text">';
                                 echo '<img class="file-icon" src="./assets/img/'.$infoFile['extension'].'.png" alt="'.$infoFile['extension'].' logo" width="30px">';
                                 if (strlen($infoFile['filename']) > 13) {
@@ -54,13 +56,13 @@
                                     echo '<div class="info-file">'; 
                                     echo '<p>File name: '.$infoFile['filename'].'</p>';
                                     echo '<p>Extension: '.$infoFile['extension'].'</p>';
-                                    if (filesize($path.'/'.$files[$i]) < 1000000) {
-                                        echo '<p>Size: '. round(((filesize($path.'/'.$files[$i]))/1000), 2) .' Kb</p>';
+                                    if (filesize($newPath) < 1000000) {
+                                        echo '<p>Size: '. round(((filesize($newPath))/1000), 2) .' Kb</p>';
                                     } else {
-                                        echo '<p>Size: '. round(((filesize($path.'/'.$files[$i]))/1000000), 2) .' Mb</p>';
+                                        echo '<p>Size: '. round(((filesize($newPath))/1000000), 2) .' Mb</p>';
                                     }
-                                    echo '<p>Created: '.date("D d M Y g:i a", filectime($path.'/'.$files[$i])).'</p>';
-                                    echo '<p>Modified: '.date("D d M Y g:i a", filemtime($path.'/'.$files[$i])).'</p>';
+                                    echo '<p>Created: '.date("D d M Y g:i a", filectime($newPath)).'</p>';
+                                    echo '<p>Modified: '.date("D d M Y g:i a", filemtime($newPath)).'</p>';
                                     echo '</div>'; 
                                 echo '</div></a>';
                             } 
