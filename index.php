6ba633f96a7e13a462621b84189ab92d9f0f3518
Daniel Wilson <danielvillenawilson@gmail.com>
2023-01-10 10:05:22 +0100

    update

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="js/script.js" defer></script>
+    <link rel="stylesheet" src="./css/style.css" type="text/css">
+</head>
+<body>
+    
+</body>
+</html>
