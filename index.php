10fa92b060123478144a2b6fcb89797e643da170
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-01-10 11:07:14 +0100

    estructura y algo de estilos

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="js/script.js" defer></script>
-    <link rel="stylesheet" src="./css/style.css" type="text/css">
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Wisus Drive</title>
+        <script src="js/script.js" defer></script>
+        <link rel="stylesheet" href="./css/style.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            <nav id="navBar">
+                <div id="companyLogo">LOGO</div>
+                <div id="userLogo">user</div>
+            </nav>
+        </header>
+        <main>
+            <div id="mainContainer">
+                <div class="mainChildren">1</div>
+                <div class="mainChildren">2</div>
+                <div class="mainChildren">3</div>
+            </div>
+        </main>
+        <footer></footer>
+    </body>
 </html>
