5d346a820a738a5c432ab8bbf237bafc46dafabc
Albert Gomis <albert.gomis.20@gmail.com>
2023-01-12 17:43:15 +0100

    Commit search 2.0

--- search.php
+++ search.php
@@ -1,14 +1,41 @@
 <?php
-function search(){
+// function search(){
     
-    foreach (glob("root/*/h*") as $nombre_fichero) {
-        echo $nombre_fichero;
+//     foreach (glob("root/h*") as $nombre_fichero) {
+//         echo $nombre_fichero;
+//     }
+//     foreach (glob("root/*/h*") as $nombre_fichero) {
+//         echo $nombre_fichero;
+//     }
+//     foreach (glob("root/*/*/h*") as $nombre_fichero) {
+//         echo $nombre_fichero;
+//     }
+
+// }
+
+// search();
+
+
+function search2($e){
+    $directoriesFolders = array();
+    foreach (glob($e) as $name_fichero){
+        $fichero = $name_fichero;
+        if (!strpos($fichero, '.')) {
+            array_push($directoriesFolders, $fichero);
+            search2($fichero . "/*");
+        } else {
+            array_push($directoriesFolders, $fichero);
+     }
+    }
+    var_dump($directoriesFolders);
+    $findings = array();
+    foreach ($directoriesFolders as $directory){
+        if (strpos($directory, "ho")){
+            array_push($findings, $directory);
+        }
     }
 
-    foreach (glob("root/h*") as $nombre_fichero) {
-        echo $nombre_fichero;
-    }
-
+    // print_r ($findings);
 }
 
-search();
+search2("root/*");
