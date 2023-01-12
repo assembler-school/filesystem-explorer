cad556043dc69232e19e5daf533b5af1d18f7fc5
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-01-12 18:03:48 +0100

    refactorizate selectElement

--- js/selectElement.js
+++ js/selectElement.js
@@ -8,46 +8,48 @@ let dataPath = "";
 
 filesPath.addEventListener("click", selectElementChildren);
 filesPath.addEventListener("click", selectElementFather);
-filesPath.addEventListener("click", printFolderTitleName);
+/* filesPath.addEventListener("click", printFolderTitleName); */
 
 function selectElementChildren(event) {
     let selectedElementChildren = event.target.parentNode;
-    console.log(selectedElementChildren);
     if (selectedElementChildren.classList.contains("first-list")) {
         let firstList = selectedElementChildren;
         firstList.style.backgroundColor = "yellow";
+        dataPath = selectedElementChildren.getAttribute('data-path'); 
+        printFolderTitleName(selectedElementChildren);
     }
 }
 function selectElementFather(event) {
     let selectedElementFather = event.target;
-    console.log(selectedElementFather);
     if (selectedElementFather.classList.contains("first-list")) {
         let firstList = selectedElementFather;
         firstList.style.backgroundColor = "yellow";
+        dataPath = selectedElementFather.getAttribute('data-path');
+        printFolderTitleName(selectedElementFather);
     }
 }
 
-function printFolderTitleName(event) {
-    let selectedElement = event.target.parentNode;
-    dataPath = selectedElement.getAttribute('data-path');
+function printFolderTitleName(selectedElement) {
+    /* let selectedElement = event.target.parentNode;
+    dataPath = selectedElement.getAttribute('data-path'); */
     console.log(dataPath);
-    printInfoFiles();
+    getInfoFiles();
     if (selectedElement.getAttribute('type') == "folder") {
         pathSecondFolderTitle.textContent = "files/" + dataPath;
-       /*  fetch("modules/printFilesSecondChild.php" + "?" + "dataPath=" + "files/" + dataPath, {
+        let dataPathWithoutSlash = dataPath.substring(0, dataPath.length-1);
+        fetch("modules/printFilesSecondChild.php" + "?" + "dataPath=" + "files/" + dataPathWithoutSlash, {
             method: "GET",
         })
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
-            getInputValue(data);
         })
-        .catch((err) => console.log("Request failed: ", err)); */
+        .catch((err) => console.log("Request failed: ", err));
     }
 }
 
-function printInfoFiles() {
-    dataPathWithoutSlash = dataPath.substring(0, dataPath.length-1);
+function getInfoFiles() {
+    let dataPathWithoutSlash = dataPath.substring(0, dataPath.length-1);
     fetch("modules/fileInfo.php" + "?" + "path=" + dataPathWithoutSlash, {
             method: "GET",
         })
