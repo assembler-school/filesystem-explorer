43275c70d8908398e14d7c975a284b4814218bb1
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-01-13 20:11:21 +0100

    Dbl click Second Child folders

--- modules/printFilesSecondChild.php
+++ modules/printFilesSecondChild.php
@@ -13,14 +13,15 @@ if(is_dir("../files/".$dataPath)){
             $fileExt = pathinfo($readFolder, PATHINFO_EXTENSION);
             $fileActualExt = strtolower($fileExt);
             $dataNewPath = $dataPath."/".$readFolder;
+            
             if($fileActualExt==false){
-                array_push($arrayPrint, "<li data-path='$dataNewPath/' type='folder'><img class='folder-second-list-img' src='images/folderIconSmallx2.png' alt='folder'><span class='text-second-list'>$readFolder</span></li>");
+                array_push($arrayPrint, "<li class='first-list' data-path='$dataNewPath/' type='folder'><img class='folder-second-list-img' src='images/folderIconSmallx2.png' alt='folder'><span class='text-second-list'>$readFolder</span></li>");
             } else {
                 $readFolderArray = explode(".", $readFolder);
                 $readFolder = reset($readFolderArray);
                 $readFolderExt = strtoupper(end($readFolderArray));
                 if($fileActualExt==="png"){
-                    array_push($arrayPrint, "<li data-path='$dataNewPath/' ><img class='folder-second-list-img' src='images/pngIcon.png' alt='folder'><span class='text-second-list'>$readFolder</span><span class='extesion-file'>$readFolderExt</span></li>");
+                    array_push($arrayPrint, "<li class='first-list' data-path='$dataNewPath/' ><img class='folder-second-list-img' src='images/pngIcon.png' alt='folder'><span class='text-second-list'>$readFolder</span><span class='extesion-file'>$readFolderExt</span></li>");
                 }
             }
         }
