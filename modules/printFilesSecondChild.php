268f0b6cafb886f3f03addc2bac98db1c989d60f
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-01-13 13:38:56 +0100

    path entire solve

--- modules/printFilesSecondChild.php
+++ modules/printFilesSecondChild.php
@@ -1,7 +1,6 @@
 <?php
 
 $dataPath = $_REQUEST["dataPathSecond"];
-/* clearstatcache(); */
 
 $arrayPrint = array();
 
@@ -13,20 +12,20 @@ if(is_dir("../files/".$dataPath)){
         if ($readFolder != "." && $readFolder != "..") {
             $fileExt = pathinfo($readFolder, PATHINFO_EXTENSION);
             $fileActualExt = strtolower($fileExt);
+            $dataNewPath = $dataPath."/".$readFolder;
             if($fileActualExt==false){
-                array_push($arrayPrint, "<li data-path='$readFolder/' type='folder'><img class='folder-second-list-img' src='images/folderIconSmallx2.png' alt='folder'><span class='text-second-list'>$readFolder</span></li>");
+                array_push($arrayPrint, "<li data-path='$dataNewPath/' type='folder'><img class='folder-second-list-img' src='images/folderIconSmallx2.png' alt='folder'><span class='text-second-list'>$readFolder</span></li>");
             } else {
                 $readFolderArray = explode(".", $readFolder);
                 $readFolder = reset($readFolderArray);
                 $readFolderExt = strtoupper(end($readFolderArray));
                 if($fileActualExt==="png"){
-                    array_push($arrayPrint, "<li data-path='$readFolder/' ><img class='folder-second-list-img' src='images/pngIcon.png' alt='folder'><span class='text-second-list'>$readFolder</span><span class='extesion-file'>$readFolderExt</span></li>");
+                    array_push($arrayPrint, "<li data-path='$dataNewPath/' ><img class='folder-second-list-img' src='images/pngIcon.png' alt='folder'><span class='text-second-list'>$readFolder</span><span class='extesion-file'>$readFolderExt</span></li>");
                 }
             }
         }
     }
     echo json_encode($arrayPrint);
-    
     closedir($openedFolder);
 } else {
     echo json_encode("The directory does not exist");
